5cf0e743f3c98322266564e361469c9cc70023ef
Elvis Tapfumanei <elvis@elmunei.com>
2018-04-02 16:04:40 +0200

    Initial Commit

---
+++ Urban Tails/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Urban Tails
+//
+//  Created by Elvis Tapfumanei on 2018/04/02.
+//  Copyright © 2018 Elvis Tapfumanei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Urban TailsTests/Urban_TailsTests.swift
@@ -0,0 +1,36 @@
+//
+//  Urban_TailsTests.swift
+//  Urban TailsTests
+//
+//  Created by Elvis Tapfumanei on 2018/04/02.
+//  Copyright © 2018 Elvis Tapfumanei. All rights reserved.
+//
+
+import XCTest
+@testable import Urban_Tails
+
+class Urban_TailsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
